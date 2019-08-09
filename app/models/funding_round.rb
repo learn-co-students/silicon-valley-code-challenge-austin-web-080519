ad2d8a9306bdc07b4712bdc6eae8690e410b30ca
Anais Ahmed <anaismahmed@uchicago.edu>
2019-08-08 21:00:55 -0500

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,17 @@
 class FundingRound
+  attr_reader :startup, :venture_capitalist, :investment
+  attr_accessor :type
+  @@all = []
+  def initialize(startup, venture_capitalist, type, investment)
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @type = type
+    @investment = investment.to_f
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,49 @@
+require "pry"
+require_relative "funding_round"
 class VentureCapitalist
+  attr_reader :name, :total_worth
+  @@all = []
+  def initialize(name, total_worth)
+    @name = name
+    total_worth = 0 if total_worth < 0
+    @total_worth = total_worth.to_f
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def total_worth=(total_worth)
+    total_worth = 0 if total_worth < 0
+    @total_worth = total_worth.to_f
+  end
+
+
+  def self.tres_commas_club
+    @@all.select{|capitalist| capitalist.total_worth > 1000000000}
+  end
+
+  def offer_contract(startup, type, amount)
+    FundingRound.new(startup, self, type, amount)
+  end
+
+  def funding_rounds
+    FundingRound.all.select{|round| round.venture_capitalist == self}
+  end
+
+  def portfolio
+    self.funding_rounds.collect{|round| round.startup}.uniq
+  end
+
+  def biggest_investment
+    self.funding_rounds.max {|round1, round2| round1.investment <=> round2.investment}
+  end
+
+  def invested(domain)
+    total = self.funding_rounds.select{|round| round.startup.domain == domain}.collect{|round| round.investment}.inject{|sum, n| sum + n}
+    total ||= 0
+  end
+
 
 end
